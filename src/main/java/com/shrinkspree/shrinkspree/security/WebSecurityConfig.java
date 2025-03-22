cdd47d92484eb6ad615f4030fcb125bda0d3d75e
Prajyot Tayde <pmtbmt@gmail.com>
2025-03-22 09:15:17 +0530

    fix cors issue

--- src/main/java/com/shrinkspree/shrinkspree/security/WebSecurityConfig.java
+++ src/main/java/com/shrinkspree/shrinkspree/security/WebSecurityConfig.java
@@ -2,12 +2,14 @@ package com.shrinkspree.shrinkspree.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -51,14 +53,16 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**")
-            .permitAll()
-            .requestMatchers("/api/urls/**").authenticated()
-            .requestMatchers("/{shortUrl}").permitAll()
-            .anyRequest().authenticated()
-        );
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/urls/**").authenticated()
+                .requestMatchers("/{shortUrl}").permitAll()
+                .anyRequest().authenticated()
 
+            );
+        http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
